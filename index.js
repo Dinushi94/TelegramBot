f6f664535632526dd97347ac7fc007ddecf06049
Dinushi Dhananjani <dinushidhananjani@gmail.com>
2020-12-29 13:56:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const functions = require('firebase-functions');
+const { Telegraf } = require('telegraf')
+
+const bot = new Telegraf('1416207322:AAEpl0w67OsJcKpv_p5kaC-cRMSOr_R2CtM')
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
+
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
